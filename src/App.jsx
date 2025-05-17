8367e11b18b6448c63c36e7350fd834d87802ab4
Leeroy Kwashire <leeroykwashire1@gmail.com>
2025-05-17 12:53:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/customers', {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      console.log('Fetched customers:', response.data);
+      setCustomers(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching customers:', error);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Customer List</h1>
+      <ul>
+        {customers.map((customer, index) => (
+          <li key={index}>{JSON.stringify(customer)}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
