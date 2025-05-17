0633f06a59ed7caab4fa6fbdf7a0a89620eff44e
Leeroy Kwashire <leeroykwashire1@gmail.com>
2025-05-17 12:58:30 +0200

    removed heading

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
   return (
     <>
-      <h1>Customer List</h1>
+  
       <ul>
         {customers.map((customer, index) => (
           <li key={index}>{JSON.stringify(customer)}</li>
